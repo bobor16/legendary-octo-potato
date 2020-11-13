fa85e4bb953c49f99d6a31fe4817b0231aec2224
bobor16 <bobor16@student.sdu.dk>
2020-11-13 13:16:14 +0100

    Added the project to the right main folder.

--- Totalview/Totalview/obj/Debug/netstandard2.0/Views/MyStatePage.xaml.g.cs
+++ Totalview/Totalview/obj/Debug/netstandard2.0/Views/MyStatePage.xaml.g.cs
@@ -17,11 +17,15 @@ namespace Totalview.View {
     public partial class MyStatePage : global::Xamarin.Forms.ContentPage {
         
         [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
+        private global::Xamarin.Forms.ContentPage MyStatePageTest;
+        
+        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
         private global::Xamarin.Forms.ListView listView;
         
         [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
         private void InitializeComponent() {
             global::Xamarin.Forms.Xaml.Extensions.LoadFromXaml(this, typeof(MyStatePage));
+            MyStatePageTest = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.ContentPage>(this, "MyStatePageTest");
             listView = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.ListView>(this, "listView");
         }
     }
---
+++ UICrossPlatformTest/Tests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using NUnit.Framework;
+using Xamarin.UITest;
+using Xamarin.UITest.Queries;
+
+namespace UICrossPlatformTest
+{
+    [TestFixture(Platform.Android)]
+    //[TestFixture(Platform.iOS)]
+    public class Tests
+    {
+        IApp app;
+        Platform platform;
+
+        public Tests(Platform platform)
+        {
+            this.platform = platform;
+        }
+
+        [SetUp]
+        public void BeforeEachTest()
+        {
+            app = AppInitializer.StartApp(platform);
+        }
+
+        [Test]
+        public void LoginPageTest()
+        {
+            app.Tap(x => x.Button("Next"));
+            var result = app.WaitForElement(e => e.Marked("get from Database"));
+            Assert.IsFalse(result.Any());
+            app.Repl();
+
+        }
+    }
+}
